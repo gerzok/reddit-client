4023511f4f17ee91791bee95fed5952ae8ee0d32
Gerzo Karim <gerzok@devhouse.io>
2021-01-23 00:48:46 -0600

    Connecting Redux with React 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RedditApp from './components/view/home.container';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  redditReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-  <RedditApp />,
+  <Provider store={store}>
+    <RedditApp />
+  </Provider>,
   document.getElementById('root')
 );
